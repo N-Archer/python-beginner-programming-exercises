3251583e90ab68a5772ca3e2c376895df6e0df38
N-Archer <archer.nj@gmail.com>
2021-06-17 01:09:01 +0000

    first commit

--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,9 @@
 import random
+    
+def generate_random():
+    r1 = random.randint(0, 9)
+    return r1
+    
+print (generate_random())
 
 # your code here
--- exercises/16-Create-A-For-Loop/app.py
+++ exercises/16-Create-A-For-Loop/app.py
@@ -1,4 +1,8 @@
 def standards_maker():
     #your code here
+    for i in range(1,301,1):
+        print("I will write questions if I am stuck")
+
+standards_maker()
 
 #remember to call the function outside (here)
